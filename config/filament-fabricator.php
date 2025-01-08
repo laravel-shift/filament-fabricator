05316577979576b5a8eeae54c3f1b2831d92f27b
Voltra <Voltra@users.noreply.github.com>
2025-01-08 04:19:06 +0100

    Replace the default middleware list by the 'web'  middleware group (#207)

--- config/filament-fabricator.php
+++ config/filament-fabricator.php
@@ -24,13 +24,7 @@ return [
     ],
 
     'middleware' => [
-        \Illuminate\Cookie\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        // \Illuminate\Session\Middleware\AuthenticateSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'web',
     ],
 
     'page-model' => \Z3d0X\FilamentFabricator\Models\Page::class,
